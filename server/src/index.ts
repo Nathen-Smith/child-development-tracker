4aa6ba3dd6897a3e13fce7f1a7b39875286abaa3
Nathen-Smith <nathencsmith@gmail.com>
2021-11-20 01:18:24 -0600

    moved to ts-node-dev

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import cors from "cors";
-const app = express();
-const port = 5000; // default port to listen
+import mongoose from "mongoose";
+import mongoKey from "../config";
+
+export const app = express();
+const port = process.env.PORT || 8080;
 app.use(cors());
+mongoose
+  .connect(process.env.MONGO_CONNECTION || mongoKey)
+  .catch((err) => console.log(err));
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-  res.send({ message: "OK", data: "Hello world!!" });
+  res.send({ message: "OK", data: "Hello world!" });
 });
 
 // start the Express server
