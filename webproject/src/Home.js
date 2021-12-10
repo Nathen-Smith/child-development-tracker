21a8d05a5145f8705fa3d410563d121473d8db59
Himanshu Minocha <minocha2@illinois.edu>
2021-12-10 00:29:26 -0600

    Home Page Layout

--- webproject/src/Home.js
+++ webproject/src/Home.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import { React, useEffect, useState } from "react";
 import PropTypes from 'prop-types';
+import { StatsCard } from './StatsCard';
+import { JournalCard } from './JournalCard';
+
 // import { useHistory } from "react-router-dom";
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,7 +13,10 @@ import PropTypes from 'prop-types';
 // import Nav from 'react-bootstrap/Nav'
 
 const axios = require('axios');
-const image1 = require('./img/image1.png'); 
+const image1 = require('./img/image1.png');
+const image2 = require('./img/image2.png');
+const image3 = require('./img/image3.png');
+
 
 
 
@@ -68,39 +74,65 @@ export function Home(props) {
 
 
 
-<div className="min-h-screen bg-white flex">
+    <div className="min-h-screen bg-white flex overflow-hidden">
       <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
         <div className="mx-auto w-full max-w-sm lg:w-96">
-        <h1 class="text-lg font-semibold text-gray-900">Child Development Monitor</h1> 
-        <div class="columns-3 mt-6">
+          <h1 class="text-lg font-semibold text-gray-900">Child Development Monitor</h1>
+          <div class="columns-3 mt-6">
             <div class="column">
               <img src={image1} alt="logo" />
             </div>
             <div class="column">
-            <div class="column">
-              <img src={image1} alt="logo" />
+              <div class="column">
+                <img src={image2} alt="logo" />
+              </div>
             </div>
-            </div>
             <div class="column">
-            <div class="column">
-              <img src={image1} alt="logo" />
-            </div>
+              <div class="column">
+                <img src={image3} alt="logo" />
+              </div>
             </div>
           </div>
         </div>
 
-        <h1 class="text-lg font-semibold text-gray-900 mt-12">Statistics</h1> 
+        <h1 class="text-lg font-semibold text-gray-900 mt-12">Statistics</h1>
+
+        <StatsCard color={'primary'} name="Social and Emotional" timeline={6} />
+        <StatsCard color={'success'} name="Language/Communication" timeline={6} />
+        <StatsCard color={'danger'} name="Cognitive Development" timeline={6} />
+        <StatsCard color={'warning'} name="Social and Emotional" timeline={6} />
 
 
       </div>
-      <div className="hidden lg:block relative w-0 flex-1">
-        <img
-          className="absolute inset-0 h-full w-full object-cover"
-          src="https://d2g27qhphzrmc0.cloudfront.net/omsWebAssets/images/joshua-earle-9idqIGrLuTE-unsplash.jpg"
-          alt=""
-        />
+      <div>
+
+      <div class="flex-auto flex flex-row-reverse mt-12 mr-12" style={{ 'height': '5vh' }}>
+        <button class="h-10 px-6 font-semibold rounded-md bg-indigo-700 text-white" type="submit">
+          New Journal Entry
+        </button>
+        <button class="h-10 px-6 font-semibold rounded-md border border-gray-200 text-gray-900 mr-6" type="button">
+          Update Milestones
+        </button>
       </div>
+        <div className='overflow-scroll pt-12' style={{ 'height': '95vh' }}>
+          <JournalCard />
+          <JournalCard />
+          <JournalCard />
+          <JournalCard />
+          <JournalCard />
+          <JournalCard />
+        </div>
+
+      </div>
+
+
+
+
     </div>
+
+
+    // </div>
+    // </div>
 
   )
 }
