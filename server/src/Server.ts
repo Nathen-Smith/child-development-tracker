b31aaed505959345a68655559bc69e27eeb9d3a0
Nathen-Smith <nathencsmith@gmail.com>
2021-12-13 07:15:52 -0600

    docker updates

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -1,21 +1,38 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import bodyParser = require('body-parser');
+import bodyParser = require("body-parser");
 
 import baseRouter from "./routes";
-import mongoKey from "../config";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.PROD === "true" ? "https://nathen-smith.gitlab.io/" : "*",
+};
+app.use(cors(corsOptions));
 app.use("/api", baseRouter);
 
-mongoose
-  .connect(process.env.MONGO_CONNECTION || mongoKey)
-  .catch((err) => console.log(err));
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://aws.amazon.com/developers/getting-started/nodejs/
+
+// Load the AWS SDK
+var AWS = require("aws-sdk"),
+  region = "us-east-1",
+  secretName = "prod/mongoDB/apiKey";
+
+// Create a Secrets Manager client
+var client = new AWS.SecretsManager({
+  region: region,
+});
+
+client.getSecretValue({ SecretId: secretName }, function (err: any, data: any) {
+  if (!err && "SecretString" in data)
+    mongoose.connect(data.SecretString).catch((err) => console.log(err));
+});
 
 // TEST ROUTE
 app.get("/api/test", (req, res) => {
