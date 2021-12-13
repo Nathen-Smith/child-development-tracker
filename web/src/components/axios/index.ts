ec5af2027e2fb8afb94ab63a8ebf0de34bb0fa0f
Nathen-Smith <nathencsmith@gmail.com>
2021-12-13 08:37:57 -0600

    node_env cant be variable name

--- web/src/components/axios/index.ts
+++ web/src/components/axios/index.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? "https://nathensmith-wpfp-server-6v9xp.ondigitalocean.app/api"
-      : "http://localhost:8080/api",
+  baseURL: process.env.PRODUCTION
+    ? "https://nathensmith-wpfp-server-6v9xp.ondigitalocean.app/api"
+    : "http://localhost:8080/api",
 });
 
 export default instance;
