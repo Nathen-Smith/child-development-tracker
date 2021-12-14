caea4aa7a9bd03d0f539bed07921e0d0c9589fac
Nathen-Smith <nathencsmith@gmail.com>
2021-12-14 04:02:04 -0600

    small fixes

--- README.md
+++
@@ -1,3 +0,0 @@
-Frontend deployment
-
-https://nathen-smith.gitlab.io/cs498rk_final/
--- server/config/index.ts
+++
@@ -1,4 +0,0 @@
-const mongoKey =
-  "mongodb+srv://nathen:NqTI83eabF2v1vg4@cluster0.znqch.mongodb.net/wp_fp?retryWrites=true&w=majority";
-
-export default mongoKey;
