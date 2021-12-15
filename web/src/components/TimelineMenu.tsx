7bf4149f97895fa65fa2deb81c281a0af27882e1
Angela Zhao <alz3@illinois.edu>
2021-12-15 12:29:45 -0600

    add dropdown component

---
+++ web/src/components/TimelineMenu.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+interface TimelineMenuProps {
+  updateFunc(months: number): any;
+}
+
+export const TimelineMenu: React.FC<TimelineMenuProps> = ({
+  updateFunc
+}) => {
+  const [selection, setSelection] = useState<number>(2);
+  const choices : Array<number> = [2, 4, 6, 9, 12, 18, 24, 36, 48, 60];
+
+  return (
+    <select value={selection} onChange={event => {
+      updateFunc(parseInt(event.target.value));
+      setSelection(parseInt(event.target.value));
+    }}>
+      {choices.map(num => <option value={num}>{num} Months</option>)}
+    </select>
+  );
+};
