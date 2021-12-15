0fcf60b09bfaa6893f0731f669321242183f0acd
Angela Zhao <alz3@illinois.edu>
2021-12-15 11:49:01 -0600

    buttons on home page work now

--- web/src/components/Home.tsx
+++ web/src/components/Home.tsx
@@ -4,6 +4,7 @@ import { JournalCard } from "./journal/JournalCard";
 import axios from "../axios";
 import { useUser } from "reactfire";
 import { HomeNoAuth } from "./HomeNoAuth";
+import { Link } from "react-router-dom";
 
 import { JournalArrProps } from "./journal";
 
@@ -34,19 +35,25 @@ export const Home = () => {
             Child Development Monitor
           </h1>
           <div className="columns-3 mt-6 flex flex-row">
-            <div className="column">
-              <img src="https://i.ibb.co/HpXknVP/image1.png" alt="logo" />
-            </div>
-            <div className="column">
+            <Link to="/food">
               <div className="column">
-                <img src="https://i.ibb.co/26zN4bM/image2.png" alt="logo" />
+                <img src="https://i.ibb.co/HpXknVP/image1.png" alt="logo" />
               </div>
-            </div>
-            <div className="column">
+            </Link>
+            <Link to="/sleep">
               <div className="column">
-                <img src="https://i.ibb.co/NtSHHYj/image3.png" alt="logo" />
+                <div className="column">
+                  <img src="https://i.ibb.co/26zN4bM/image2.png" alt="logo" />
+                </div>
               </div>
-            </div>
+            </Link>
+            <Link to="/changes">
+              <div className="column">
+                <div className="column">
+                  <img src="https://i.ibb.co/NtSHHYj/image3.png" alt="logo" />
+                </div>
+              </div>
+            </Link>
           </div>
         </div>
 
@@ -68,18 +75,22 @@ export const Home = () => {
           className="flex-auto flex flex-row-reverse mt-4 mr-12"
           style={{ height: "5vh" }}
         >
-          <button
-            className="h-10 px-6 font-semibold rounded-md bg-indigo-700 text-white"
-            type="submit"
-          >
-            New Journal Entry
-          </button>
-          <button
-            className="h-10 px-6 font-semibold rounded-md border border-gray-200 text-gray-900 mr-6"
-            type="button"
-          >
-            Update Milestones
-          </button>
+          <Link to="/journal/post">
+            <button
+              className="h-10 px-6 font-semibold rounded-md bg-indigo-700 text-white"
+              type="submit"
+            >
+              New Journal Entry
+            </button>
+          </Link>
+          <Link to="/milestones">
+            <button
+              className="h-10 px-6 font-semibold rounded-md border border-gray-200 text-gray-900 mr-6"
+              type="button"
+            >
+              Update Milestones
+            </button>
+          </Link>
         </div>
         <div className="overflow-scroll pt-12" style={{ height: "88vh" }}>
           {journals?.map(({ title, createdAt, body }, idx) => {
