c2007af72bee3f52c3e4f19e085dcc343fdf7ed3
Neha <ngogate10@gmail.com>
2021-12-15 11:17:42 -0600

    improved non auth home page ui

--- web/src/components/HomeNoAuth.tsx
+++ web/src/components/HomeNoAuth.tsx
@@ -1,3 +1,18 @@
+import React from "react";
+import { JournalCard } from "./journal/JournalCard";
+
+import Badge from "react-bootstrap/Badge";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+
+
 export const HomeNoAuth = () => {
-  return <div>no auth needed</div>;
+  return <div >
+    <body style={{backgroundColor:"#fffbcf", height: '100vh'}}>
+      <div style={{padding:"200px", color: "#021e6e", fontSize:'4em', textAlign: "center", backgroundColor:"#d9fcfc"}  }>Child Development Tracker {"\u2600"}</div> 
+    </body>
+    </div>;
 };
